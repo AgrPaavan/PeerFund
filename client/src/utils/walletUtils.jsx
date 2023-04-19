5ff76789ccb183ac908a7ed8b77c65d935ea10db
Paavan Agrawal <33314332+AgrPaavan@users.noreply.github.com>
2023-04-19 14:03:47 +0530

    Make disconnect work properly

--- client/src/utils/walletUtils.jsx
+++ client/src/utils/walletUtils.jsx
@@ -1,13 +1,30 @@
-import { setGlobalState } from "../store";
+import { getGlobalState, setGlobalState } from "../store";
 import { loadCampaigns } from "./campaignUtils";
 const { ethereum } = window;
 
 const connectWallet = async () => {
+  const connectedAccount = getGlobalState("connectedAccount");
   try {
     if (!ethereum) return alert("Please install Metamask");
-    const accounts = await ethereum.request({ method: "eth_requestAccounts" });
-    setGlobalState("connectedAccount", accounts[0]?.toLowerCase());
-    await loadCampaigns();
+    if (typeof connectedAccount === "undefined") {
+      const accounts = await window.ethereum
+        .request({
+          method: "wallet_requestPermissions",
+          params: [
+            {
+              eth_accounts: {},
+            },
+          ],
+        })
+        .then(() =>
+          ethereum.request({
+            method: "eth_requestAccounts",
+          }),
+        );
+
+      setGlobalState("connectedAccount", accounts[0]?.toLowerCase());
+      await loadCampaigns();
+    }
   } catch (error) {
     reportError(error);
   }
